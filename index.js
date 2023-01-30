c5291c4b6047df3618dd964da0f31037c0533d70
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-01-30 22:58:46 +0000

    added comments to my code and fixed typo in Engineer class

--- index.js
+++ index.js
@@ -10,74 +10,84 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
+// Storing an empty array as a variable so I can save the Manager's, Engineer and Intern's details into here
 const employeeDetails = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+// This function will present the Manager's questions and save the responses
 function generateManager() {
     return inquirer.prompt([{
         name: 'managerName',
-        message: "Please enter the Team Manager's name.",
-        type: 'input'
+        message: 'Who is the team manager?'
     },
     {
         name: 'managerID',
-        message: 'Please their Employee ID'
+        message: 'Please enter their Employee ID:'
     },
     {
         name: 'managerEmail',
-        message: 'Please enter their email address.'
+        message: 'What is their email?'
     },
     {
         name: 'officeNumber',
-        message: 'Please enter the office number'
+        message: 'Please enter the office number:'
     }]).then(data => {
+        // Here the data is being pushed into the stored array and a new Manager object created with the responses provided
         employeeDetails.push(new Manager(data.managerName, data.managerID, data.managerEmail, data.officeNumber));
     })
 }
 
+
+// This function will present the questions for the Engineer when selected and save the responses
 function generateEngineer() {
     return inquirer.prompt([{
         name: 'engineerName',
-        message: "Please enter the Engineer's name."
+        message: 'What is the name of the Engineer?'
     },
     {
         name: 'engineerID',
-        message: 'Please their Employee ID'
+        message: 'Please enter their Employee ID:'
     },
     {
         name: 'engineerEmail',
-        message: 'Please enter their email address.'
+        message: 'What is their email?'
     },
     {
         name: 'gitHubID',
-        message: 'Please enter their GitHub username'
+        message: 'What is their GitHub username?'
     }]).then(data => {
-        employeeDetails.push(data);
+        // Pushing data into stored array and creating a new Engineer object from the responses
+        employeeDetails.push(new Engineer(data.engineerName, data.engineerID, data.engineerEmail, data.gitHubID));
     })
 }
 
+
+// This will present questions for the Intern when selected and save the responses
 function generateIntern() {
     return inquirer.prompt([{
         name: 'internName',
-        message: "Please enter the intern's name."
+        message: "What is the Intern's name?"
     },
     {
         name: 'internID',
-        message: 'Please their Employee ID'
+        message: 'Please enter their Employee ID:'
     },
     {
         name: 'internEmail',
-        message: 'Please enter their email address.'
+        message: 'What is their email?'
     },
     {
         name: 'school',
-        message: 'Please enter their school name'
+        message: 'What school are they attending?'
     }]).then(data => {
-        employeeDetails.push(data);
+        // Pushing data into stored array and creating a new Intern object
+        employeeDetails.push(new Intern(data.internName, data.internID, data.internEmail, data.school));
     })
 }
 
+
+// This function will display the main menu once the manager section has been completed. It allows the user to choose to add an Engineer, Intern or Finish and exit the app
 function showMainMenu() {
     return inquirer.prompt({
         type: 'list',
@@ -98,6 +108,7 @@ function showMainMenu() {
             }
         ]
     }).then(choice => {
+        // Conditional statements to determine whether Engineer or Intern function is run next
         if (choice.options === 'engineer') return generateEngineer().then(showMainMenu);
         if (choice.options === 'intern') return generateIntern().then(showMainMenu);
 
@@ -105,13 +116,19 @@ function showMainMenu() {
 }
 
 
+// This is the initial function starting the app
 function init() {
     console.log('----Welcome to the Course Manager App!-----');
 
+    // Setting the order in which the functions need to run:
     generateManager().then(showMainMenu).then(() => {
         console.log(employeeDetails);
+
+        // Storing a variable with the render function which will allow the saved array to be added to the html file that is being generated
         const html = render(employeeDetails);
         console.log(html);
+
+        //Creating the html file 
         fs.writeFile('./output/team.html', html, (error) => {
             if (error) {
                 return console.log(error);
